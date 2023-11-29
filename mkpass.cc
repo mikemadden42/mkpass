54ce3a98ab360efb5894775ce5c6f400a67d24b7
Michael Madden <mikemadden42@icloud.com>
2023-11-29 05:27:56 -0600

    Use size_t instead of int.

--- mkpass.cc
+++ mkpass.cc
@@ -7,7 +7,7 @@
 // https://stackoverflow.com/questions/47977829/generate-a-random-string-in-c11
 // g++ -std=c++17 -Wall -Wextra -pedantic -o mkpass mkpass.cc
 
-std::string random_string(int size) {
+std::string random_string(size_t size) {
     std::string str(
         "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
 
